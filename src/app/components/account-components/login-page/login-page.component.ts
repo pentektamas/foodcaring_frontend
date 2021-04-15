eecc441e6396a686034676a6b76e2256b74ead6a
Corches Sergiu <corchessergiu194@gmail.com>
2021-04-16 01:08:17 +0300

    final

--- src/app/components/account-components/login-page/login-page.component.ts
+++ src/app/components/account-components/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -25,7 +25,14 @@ export class LoginPageComponent implements OnInit {
       this.router.navigate(['mainPage']);
     },
       (error: any) => {
-      
+        Swal.fire({
+          title: 'Alert message',
+          text: 'Incorrect credentials. Please try again.',
+          icon: 'error',
+          showCancelButton: true,
+          width: '500px',
+        })
+  
       })
   
   }
